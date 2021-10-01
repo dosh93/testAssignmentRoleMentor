5fcc8a67b343ffd313d14aa3415142cca7ad51a4
dosh93 <rmatyuk@yandex.ru>
2021-10-01 16:40:56 +0300

    fix

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import enums.CargoDimensions;
-import enums.WorkloadDeliveryService;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Delivery.getCostDelivery(2, null, true, WorkloadDeliveryService.VERY_HIGH));
-    }
-}
